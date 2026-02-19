18ebe97ead0d77eea4b03943f591f1783916ae85
borigorla vekata narendra <narendraborigorla5@gmail.com>
2026-02-19 20:04:12 +0530

    Add branch_comands.py and multi_operators.py

---
+++ branch_comands.py/multi_operators.py
@@ -0,0 +1,51 @@
+#🟢 EASY LEVEL
+"""
+    1️⃣ Salary Bonus Check
+Take salary and years_of_service as input.
+Add ₹5000 bonus using += if salary > 30000 AND years_of_service ≥ 3.
+Print final salary.
+    """
+salary =input("Enter salary: ")
+year_of_service =input("Enter year of service: ")
+bonus = 5000
+sum += bonus
+if sum and salary>30000 and year_of_service>=3 :
+    print("Final salary")
+else:
+    print("Not eligible for salary")
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
